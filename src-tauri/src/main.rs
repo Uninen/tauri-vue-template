90522e4e34c4f126e6c54e51a3fff6b7f07ee7b5
Ville Säävuori <ville@unessa.net>
2022-06-16 16:12:18 +0300

    refactor: use native default menu

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -4,7 +4,7 @@
 )]
 
 use tauri::api::shell;
-use tauri::{CustomMenuItem, Manager, Menu, MenuEntry, MenuItem, Submenu};
+use tauri::{CustomMenuItem, Manager, Menu, Submenu};
 
 #[tauri::command]
 fn backend_add(number: i32) -> i32 {
@@ -16,26 +16,8 @@ fn main() {
     let ctx = tauri::generate_context!();
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![backend_add])
-        .menu(Menu::with_items([
-            #[cfg(target_os = "macos")]
-            MenuEntry::Submenu(Submenu::new(
-                &ctx.package_info().name,
-                Menu::with_items([
-                    // Commented until https://github.com/tauri-apps/tauri-docs/issues/537 is fixed
-                    // MenuItem::About(ctx.package_info().name.clone(), {}).into(),
-                    // MenuItem::Separator.into(),
-                    MenuItem::Hide.into(),
-                    MenuItem::HideOthers.into(),
-                    MenuItem::ShowAll.into(),
-                    MenuItem::Separator.into(),
-                    MenuItem::Quit.into(),
-                ]),
-            )),
-            MenuEntry::Submenu(Submenu::new(
-                "Window",
-                Menu::with_items([MenuItem::Minimize.into(), MenuItem::Zoom.into()]),
-            )),
-            MenuEntry::Submenu(Submenu::new(
+        .menu(
+            tauri::Menu::os_default("Tauri Vite Template").add_submenu(Submenu::new(
                 "Help",
                 Menu::with_items([CustomMenuItem::new(
                     "Online Documentation",
@@ -43,7 +25,7 @@ fn main() {
                 )
                 .into()]),
             )),
-        ]))
+        )
         .on_menu_event(|event| {
             let event_name = event.menu_item_id();
             match event_name {
