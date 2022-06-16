b14ba2cc44d2e9a9f2d99946679ecb5b836155b7
Ville Säävuori <ville@unessa.net>
2022-06-16 14:37:16 +0300

    chore: tweakk HelloWord wordings

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -31,11 +31,12 @@ async function backendAdd() {
   <div class="w-full space-x-2">
     <button type="button" class="btn" @click="count++">Add 1</button>
 
-    <button type="button" class="btn" @click="backendAdd">Add 1 in backend</button>
+    <button type="button" class="btn" @click="backendAdd">Add 2 in backend</button>
   </div>
   <p>
     Edit
-    <code>components/HelloWorld.vue</code> to test hot module replacement.
+    <code>src/components/HelloWorld.vue</code> to test hot module replacement. Find the backend
+    function from <code>src-tauri/src/main.rs</code>.
   </p>
 </template>
 
