9a074b5d6ec64aa37694e762fd73e584c2237286
Ville Säävuori <ville@unessa.net>
2022-09-28 15:39:57 +0300

    docs: documented command blocking

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -8,6 +8,8 @@ use tauri::{CustomMenuItem, Manager, Menu, Submenu};
 
 #[tauri::command]
 fn backend_add(number: i32) -> i32 {
+    // Note: these commands block the main thread and hang the UI until they return.
+    // If you need to run a long-running task, use async command instead.
     println!("Backend was called with an argument: {}", number);
     number + 2
 }
